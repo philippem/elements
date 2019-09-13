9a21648ea6dbf25a1c8f44f996b196181c12f6e6
Russell O'Connor <roconnor@blockstream.io>
2019-09-13 14:01:17 -0400

    Squashed 'src/simplicity/' content from commit 0db68c5a6
    git-subtree-dir: src/simplicity
    git-subtree-split: 0db68c5a6dfb7cf54d110f566e20ff1f55098d58

---
+++ Simplicity.Coq.nix
@@ -0,0 +1,17 @@
+{ coq, lib, vst, stdenv }:
+stdenv.mkDerivation {
+  name = "Simplicity-coq-0.0.0";
+  src = lib.sourceFilesBySuffices
+      (lib.sourceByRegex ./Coq ["Simplicity" "Simplicity/.*" "Util" "Util/.*"])
+    [".v"];
+  postConfigure = ''
+    shopt -s globstar
+    coq_makefile -Q Simplicity Simplicity -Q Util Util **/*.v > Makefile
+  '';
+  buildInputs = [ coq ];
+  propagatedBuildInputs = [ vst ];
+  installFlags = "COQLIB=$(out)/lib/coq/${coq.coq-version}/";
+  meta = {
+    license = lib.licenses.mit;
+  };
+}
